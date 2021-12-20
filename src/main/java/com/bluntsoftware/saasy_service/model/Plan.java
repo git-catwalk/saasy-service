b10edc64b38dd9ac72a2a159dd0fb75b57b377c9
mcknight816 <mcknight816@gmail.com>
2021-12-20 11:37:52 -0500

    add more to the plan

--- src/main/java/com/bluntsoftware/saasy_service/model/Plan.java
+++ src/main/java/com/bluntsoftware/saasy_service/model/Plan.java
@@ -5,14 +5,17 @@ import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.Data;
 import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Plan {
-
 	private String name;
+	private String description;
+	private String planId;
 	private BigDecimal monthly;
 	private BigDecimal yearly;
+	private List<String> features;
 }
